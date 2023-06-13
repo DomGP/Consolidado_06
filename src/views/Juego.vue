1d032e81087903cb936d6d7d5e6bb5ba427570c1
DomGP <dominique.gajardoap@gmail.com>
2023-06-12 20:21:32 -0400

    Mensaje descriptivo del commit

--- src/views/Juego.vue
+++ src/views/Juego.vue
@@ -93,4 +93,9 @@ export default {
         box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
         color:white;
     }
+    img{
+        width:284px;
+        height: 284px;
+        object-fit: cover;
+    }
 </style>
